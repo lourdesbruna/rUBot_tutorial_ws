31225d7a266f51e3fbb253f7d58da28eb2ab2315
lourdesbruna <lbrunamo57@alumnes.ub.edu>
2022-02-26 20:41:04 +0100

    pingpong final version

--- src/ros_basics/scripts/pong.py
+++ src/ros_basics/scripts/pong.py
@@ -5,14 +5,14 @@ from std_msgs.msg import String
 def callback(data):
 	msg = String()
 	if data.data == 'Ping':
-    		#rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    		msg.data = 'Pong' 
-    		pub.publish(msg)
-    		rospy.loginfo(msg.data)
-    	#else:
-    		#msg.data = 'Failed!' 
-    		#pub.publish(msg)
-    		#rospy.loginfo(msg.data)
+		#rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+		msg.data = 'Pong'
+		pub.publish(msg)
+		rospy.loginfo(msg.data)
+	else:
+		msg.data = 'Failed!'
+		pub.publish(msg)
+		rospy.loginfo(msg.data)
 
 rospy.init_node('pong_node', anonymous=True)
 pub = rospy.Publisher("/pong", String, queue_size=10)
